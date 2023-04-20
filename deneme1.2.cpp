7d08d6d605ad9c56256c6e29aa6cee4e9a320f23
4as12 <90792374+4as12@users.noreply.github.com>
2023-04-21 00:21:41 +0300

    Add files via upload

---
+++ deneme1.2.cpp
@@ -0,0 +1,27 @@
+﻿// deneme1.2.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+
+int main()
+{
+    int a,i,b;//uygun degişkenleri 
+    b = 0;//b sıfıra eşit olmazsa sonuç anlamsız cikabilir çünkü hafızada rasgele bir deger olabilir
+    std::cout << "bir sayi giriniz ";//toplamını bulmak istedigimiz sayıyı bulmalıyız
+    std::cin >> a;
+    for (i = a;i > 0;i--) {
+        b = i + b;
+    }
+    std::cout << "result ==" << b;//işlem tamamlanır ve sonuç çıkarılır 
+}
+
+// Programı çalıştır: Ctrl + F5 veya Hata Ayıkla > Hata Ayıklamadan Başlat menüsü
+// Programda hata ayıkla: F5 veya Hata Ayıkla > Hata Ayıklamayı Başlat menüsü
+
+// Kullanmaya Başlama İpuçları: 
+//   1. Dosyaları eklemek/yönetmek için Çözüm Gezgini penceresini kullanın
+//   2. Kaynak denetimine bağlanmak için Takım Gezgini penceresini kullanın
+//   3. Derleme çıktısını ve diğer iletileri görmek için Çıktı penceresini kullanın
+//   4. Hataları görüntülemek için Hata Listesi penceresini kullanın
+//   5. Yeni kod dosyaları oluşturmak için Projeye Git > Yeni Öğe ekle veya varolan kod dosyalarını projeye eklemek için Proje > Var Olan Öğeyi Ekle adımlarını izleyin
+//   6. Bu projeyi daha sonra yeniden açmak için Dosya > Aç > Proje'ye gidip .sln uzantılı dosyayı seçin
